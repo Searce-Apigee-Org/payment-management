d35b5ccb58bf25c4877e4aef0a753d6fc73a85ed
anishabhujbal3010 <anisha.bhujbal@searce.com>
2026-03-19 04:24:14 +0530

    test

--- src/plugins/globalDependenciesPlugin.js
+++ src/plugins/globalDependenciesPlugin.js
@@ -55,10 +55,8 @@ const globalDependenciesPlugin = {
       paymentService,
       amaxService,
       productOrderingService,
-      refundService,
       invokeLambda,
       channelConfig,
-      raven,
     } = options;
 
     // models
@@ -85,7 +83,6 @@ const globalDependenciesPlugin = {
     server.decorate('request', 'globeOnline', globeOnline);
     server.decorate('request', 'transactions', transactions);
     server.decorate('request', 'channelConfig', channelConfig);
-    server.decorate('request', 'raven', raven);
 
     // clients
     server.decorate('request', 'secret', secret);
@@ -138,7 +135,6 @@ const globalDependenciesPlugin = {
     );
     server.decorate('request', 'oneApiService', oneApiService);
     server.decorate('request', 'paymentService', paymentService);
-    server.decorate('request', 'refundService', refundService);
 
     //data providers
     server.decorate(
